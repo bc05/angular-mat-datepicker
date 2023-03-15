7aefe19f9005e4ffffa37f9289e459fe1568c0dd
bc05 <odilon.garcez.moraes@gmail.com>
2023-03-15 17:37:02 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,10 @@ import {
 
 export const MY_FORMATS = {
   parse: {
-    dateInput: 'LL',
+    dateInput: 'DD/MM/YYYY',
   },
   display: {
     dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
   },
 };
 
